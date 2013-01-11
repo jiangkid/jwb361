c172aeb36c8b3763cee03af5379cfed49001af58
jwb361@gmail.com <jwb361@gmail.com@375f031d-d570-a5d7-2e2d-9f640cc586a3>
2013-01-11 12:01:41 +0000

    简化melp_MSVQ.m文件的LSF多级矢量量化过程

--- MELP600/melp_MSVQ.m
+++ MELP600/melp_MSVQ.m
@@ -7,23 +7,28 @@
 function g=melp_MSVQ(lpcs,f)
 global stage1;
 global stage2;
-for j=1:10													%Get the Weighs
-    w(j)=1+exp(-1i*f(j)*(1:10))*lpcs';%???
+w = zeros(1,10);
+for j=1:10 %Get the Weighs
+    w(j)=1+exp(-1i*f(j)*(1:10))*lpcs'; 
 end
 %w=abs(w);
 %w=w.^0.3;
-w=10*log10(abs(w).^2);%modify by jiangwenibn
+w=abs(w).^2;
 w=w.^(-0.3);
 w(9)=w(9)*0.64;
 w(10)=w(10)*0.16;
 
+JUDGE = 100000000;
 %d(m,1) is the judgement.
 %d(m,2:11) is the minus of the vector and the codeword
 %d(m,12:15) is the codeword.
-d(1:8,1:12)=10000000;
+d(1:8,1:12)=JUDGE;
 for s=1:128
     delta=f-stage1((s-1)*10+1:s*10);
     temp=w*(delta.^2)';
+    if temp > JUDGE
+         error('temp error: %d', temp);
+    end
     m=1;
     while m<9
         if temp<d(m,1)
@@ -37,34 +42,17 @@ for s=1:128
     end
 end
 %第一级量化完成后，矩阵根据第一列数据从小到大排列
+
 for s=1:3
     e=d;
-    d(1,2:11)=e(1,2:11)-stage2(s,1:10);
-    d(1,1)=w*(d(1,2:11).^2)';
-    d(1,12:12+s)=[e(1,12:11+s),1];
-    for m=2:8
-        delta=e(m,2:11)-stage2(1,1:10);
-        temp=w*(delta.^2)';
-        for num=1:(m-1)
-            if temp<d(num,1)
-                d(num+1:9,:)=d(num:8,:);
-                d(num,1)=temp;
-                d(num,2:11)=delta;
-                d(num,12:11+s)=e(1,12:11+s);
-                d(num,12+s)=1;
-                break;
-            end
-        end
-        if temp>=d(m-1,1)
-            d(m,2:11)=delta;
-            d(m,1)=temp;
-            d(m,12:12+s)=[e(1,12:11+s),1];
-        end
-    end
+    d(1:8,1:12)=JUDGE;    
     for j=1:8
-        for k=2:64
+        for k=1:64
             delta=e(j,2:11)-stage2(s,(k-1)*10+1:k*10);
-            temp=w*(delta.^2)';
+            temp=w*(delta.^2)';            
+            if temp > JUDGE
+                error('temp error: %d', temp);
+            end
             for n=1:8
                 if temp<d(n,1)
                     d(n+1:9,:)=d(n:8,:);
